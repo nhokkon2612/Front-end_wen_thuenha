01d68b0b6fd9c378f853d1f7de722c818461b7b5
cuong <hahuycuong1002@gmail.com>
2021-11-02 08:30:09 +0700

    add

---
+++ src/app/layouts/page/fullwidth2/fullwidth2.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-fullwidth2',
+  templateUrl: './fullwidth2.component.html',
+  styleUrls: ['./fullwidth2.component.css']
+})
+export class Fullwidth2Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
