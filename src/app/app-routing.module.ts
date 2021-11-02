3e90fceb05d8cb69917abdb06ce77a203f3b25ac
cuong <hahuycuong1002@gmail.com>
2021-11-02 13:31:48 +0700

    add

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './layouts/customers/login/login.component';
+import { ContentComponent } from './layouts/page/content/content.component';
 import { MasterComponent } from './layouts/page/master/master.component';
 
 const routes: Routes = [{
   path: '',
-  component: MasterComponent,
-  children: [{
-    path: 'login',
-    component: LoginComponent
-  }]
-}]
+  component: MasterComponent
+},
+{
+  path: 'login',
+  component: LoginComponent
+}
+]
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
