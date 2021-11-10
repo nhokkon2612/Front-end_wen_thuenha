f374f9d80431af485c893b43ca99900265943d68
cuong <hahuycuong1002@gmail.com>
2021-11-10 14:18:44 +0700

    update user ,contract

--- src/app/layouts/customers/user-accout/user-accout.component.ts
+++ src/app/layouts/customers/user-accout/user-accout.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import {HouseService} from "../../../sevices/house.service";
-import {ActivatedRoute} from "@angular/router";
+import {ActivatedRoute, Router} from "@angular/router";
 import {AuthService} from "../../../sevices/auth.service";
+import { RegisterService } from 'src/app/sevices/register.service';
+import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
+import Swal from "sweetalert2";
 
 @Component({
   selector: 'app-user-accout',
@@ -12,10 +15,19 @@ export class UserAccoutComponent implements OnInit {
   isCheckLogin = false;
   image='';
   user: any;
-
+  form: FormGroup ;
   constructor(private houseService: HouseService,
-              private router: ActivatedRoute,
-              private authService: AuthService) {
+              private routers: ActivatedRoute,
+              private router:Router,
+              private authService: AuthService,
+              private updateUserService: RegisterService,
+              private fb: FormBuilder) {
+    this.form = this.fb.group({
+      name: new FormControl(null, [Validators.required, Validators.minLength(6)]),
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      password: new FormControl(null, [Validators.required, Validators.minLength(6), Validators.maxLength(13)]),
+      confirmPassword: new FormControl(null, [Validators.required])
+    })
   }
 
   ngOnInit(): void {
@@ -24,6 +36,21 @@ export class UserAccoutComponent implements OnInit {
     }
     // @ts-ignore
     this.getUser();
+  }
+
+  submit() {
+    let data = this.form.value;
+    this.updateUserService.register(data).subscribe(
+      res => {
+        if (res.status == 'success') {
+          // Swal.fire('Đăng ký thành công','Vui lòng đăng nhập hệ thống','success')
+          Swal.fire('Đăng ký thành công','Vui lòng đăng nhập hệ thống','success')
+          this.router.navigate(['contract']);
+        } else {
+          this.router.navigate([''])
+        }
+      }
+    );
   }
 
   getUser() {
